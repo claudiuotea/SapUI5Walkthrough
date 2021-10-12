dbbb217e903359111de043e94a6b2128885a7da6
Claudiu Otea <claudiu_otea@yahoo.com>
2021-10-12 14:14:48 +0300

    Step 17 - Fragment Callbacks

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,30 +1,35 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/core/Fragment"
- ], function (Controller, MessageToast, Fragment) {
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
+  function (Controller, MessageToast, Fragment) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
-       onShowHello : function () {
-          // read msg from i18n model
-          var oBundle = this.getView().getModel("i18n").getResourceBundle();
-          var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-          var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-          // show message
-          MessageToast.show(sMsg);
-       },
+      onShowHello: function () {
+        // read msg from i18n model
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        // show message
+        MessageToast.show(sMsg);
+      },
 
-       onOpenDialog : function () {
-
+      onOpenDialog: function () {
         // create dialog lazily
         if (!this.pDialog) {
-            this.pDialog = this.loadFragment({
-                name: "sap.ui.demo.walkthrough.view.HelloDialog"
-            });
-        } 
-        this.pDialog.then(function(oDialog) {
-            oDialog.open();
-        });
+          this.pDialog = this.loadFragment({
+            name: "sap.ui.demo.walkthrough.view.HelloDialog",
+          });
         }
+        this.pDialog.then(function (oDialog) {
+          oDialog.open();
+        });
+      },
+
+      onCloseDialog: function () {
+          //note: no need to chain to the pDialog promise, cause this is calle from within the dialog
+          this.byId("helloDialog").close();
+      },
     });
- });
+  }
+);
